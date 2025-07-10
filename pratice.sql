079c6fc2a5a6e3bc1b8667b246586b791c7d068f
trangnguyen36 <trangnguyen03062004@gmail.com>
2025-07-10 16:43:18 +0700

    Update pratice.sql

--- pratice.sql
+++ pratice.sql
@@ -12,14 +12,16 @@ FROM
     OITM;
 
 --Task 2
-SELECT a.Id, b.InvoiceCode,c.DocCode, b.CardId, b.CardName, b.DocDate, b.ObjType,
+SELECT a.Id, b.InvoiceCode,c.DocCode, b.CardId, b.CardName, b.DocDate, 
+        CONVERT(DATE, b.DocDate) AS Date,
+        b.ObjType,
 CASE 
         WHEN b.ObjType = 13 THEN 'Ban hang'
         WHEN b.ObjType = 19 THEN 'Doi tra hang'
         ELSE 'Dat hang'
     END AS ObjTypeName,
 b.BranchId, b.Creator, c.CounterId, a.ItemId, c.PaymentType, c.PaymentAmount,
-c.PaymentAmount*a.Quantity AS Linetotal,
+a.LineTotal,
 a.Quantity,
 a.WeightGold,
 CASE 
@@ -32,15 +34,25 @@ JOIN Payment AS c ON b.ID=c.DocId
 WHERE b.ObjType IN (13, 19, 26);
 
 --Task 3
-WITH updated_price AS(
-SELECT b.Date, b.Time, a.goldCodeName, a.sellPrice, a.BuyPrice,
-ROW_NUMBER() OVER(PARTITION BY a.goldCodeName, b.Date ORDER BY b.Time DESC) AS rank
-FROM GoldPriceListLine AS a
-JOIN GoldPriceList AS b ON a.FatherId=b.Id)
-
-SELECT DATEADD(HOUR, 7, Date) AS updated_Date,
-       DATEADD(HOUR, 7, Time) AS updated_Time,
-       goldCodeName, sellPrice, BuyPrice 
-FROM updated_price
-WHERE rank=1;
+SELECT 
+    DATEADD(HOUR, 7, sub.Date) AS updated_Date,
+    DATEADD(HOUR, 7, sub.Time) AS updated_Time,
+    sub.goldCodeName, 
+    sub.sellPrice, 
+    sub.BuyPrice 
+FROM (
+    SELECT 
+        b.Date, 
+        b.Time, 
+        a.goldCodeName, 
+        a.sellPrice, 
+        a.BuyPrice,
+        ROW_NUMBER() OVER (
+            PARTITION BY a.goldCodeName, YEAR(b.Time), MONTH(b.Time), DAY(b.Time)
+            ORDER BY b.Time DESC
+        ) AS rank
+    FROM GoldPriceListLine AS a
+    JOIN GoldPriceList AS b ON a.FatherId = b.Id
+) AS sub
+WHERE sub.rank = 1;
 
